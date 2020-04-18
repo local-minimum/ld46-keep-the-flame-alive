a591bd22f18c94a1459ae9c6393279a7f3a5d311
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-04-18 20:14:43 +0200

    Character can turn

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class RobotController : MonoBehaviour
 {
+    [SerializeField] Tile spawnTile;
+    Tile tile;
+    TileEdge facing;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tile = spawnTile;
+        transform.position = tile.RestPosition.position;
+        facing = spawnTile.SpawnHeading;
+        transform.LookAt(facing.transform, Vector3.up);
     }
-
+    
     private void OnEnable()
     {
         RemoteController.OnSendCommand += RemoteController_OnSendCommand;
@@ -22,6 +29,23 @@ public class RobotController : MonoBehaviour
 
     private void RemoteController_OnSendCommand(RobotCommand command, float nextCommandInSeconds)
     {
-        Debug.Log(string.Format("Robot: {0}", command));
+        switch (command)
+        {
+            case RobotCommand.TurnLeftTwo:
+                facing = tile.Left(facing);
+                facing = tile.Left(facing);
+                break;
+            case RobotCommand.TurnLeft:
+                facing = tile.Left(facing);
+                break;
+            case RobotCommand.TurnRightTwo:
+                facing = tile.Right(facing);
+                facing = tile.Right(facing);
+                break;
+            case RobotCommand.TurnRight:
+                facing = tile.Right(facing);
+                break;
+        }
+        transform.LookAt(facing.transform, Vector3.up);
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Transform restPosition;
+    public Transform RestPosition
+    {
+        get { return restPosition; }
+    }
+    [SerializeField] TileEdge[] entries;
+    [SerializeField] TileEdge[] exits;
+    [SerializeField] TileEdge spawnHeading;
+    public TileEdge SpawnHeading
+    {
+        get { return spawnHeading;  }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (entries.Length != exits.Length) Debug.LogWarning(string.Format("{0} has entry exit missmatch", name));
+    }
+
+    public TileEdge Left(TileEdge heading) {
+        for (int i=0; i<exits.Length; i++)
+        {
+            if (exits[i] == heading)
+            {
+                return exits[i == 0 ? exits.Length - 1 : i - 1];
+            }
+        }
+        throw new MissingComponentException();
+    }
+
+    public TileEdge Right(TileEdge heading)
+    {
+        for (int i = 0; i < exits.Length; i++)
+        {
+            if (exits[i] == heading)
+            {
+                return exits[i == exits.Length - 1 ? 0 : i + 1];
+            }
+        }
+        throw new MissingComponentException();
+    }
+}
