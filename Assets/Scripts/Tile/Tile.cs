cdb88e0c9d7500f2a080e2faa9d5cb030348438e
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-04-19 10:43:06 +0200

    Easier adding of tiles

---
+++ Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Transform restPosition;
+    public Transform RestPosition
+    {
+        get { return restPosition; }
+    }
+    [Header("Edges")]
+    [SerializeField] TileEdge[] entries;
+    [SerializeField] TileEdge[] exits;
+    [SerializeField] bool FixEdges;
+    [Header("Only relevant for spawning")]
+    [SerializeField] TileEdge spawnHeading;
+    
+    public TileEdge SpawnHeading
+    {
+        get { return spawnHeading;  }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (entries.Length != exits.Length) Debug.LogWarning(string.Format("{0} has entry exit missmatch", name));
+    }
+
+    public TileEdge Left(TileEdge heading, int steps=1) {
+        for (int i=0; i<exits.Length; i++)
+        {
+            if (exits[i] == heading)
+            {
+                return exits[i - steps < 0 ? exits.Length + (i - steps)  : i - steps];
+            }
+        }
+        Debug.LogError(string.Format("Looking for Left from {0} on {1}", heading.name, name));
+        throw new MissingComponentException();
+    }
+
+    public TileEdge Right(TileEdge heading, int steps=1)
+    {
+        for (int i = 0; i < exits.Length; i++)
+        {
+            if (exits[i] == heading)
+            {
+                return exits[i + steps >= exits.Length ? i + steps - exits.Length : i + steps];
+            }
+        }
+        Debug.LogError(string.Format("Looking for Right from {0} on {1}", heading.name, name));
+        throw new MissingComponentException();
+    }
+
+    public TileEdge Forward(TileEdge entry)
+    {
+        for (int i = 0; i < entries.Length; i++)
+        {
+            if (entries[i] == entry)
+            {
+                return exits[i];
+            }
+        }
+        Debug.LogError(string.Format("Looking for Forward from {0} on {1}", entry.name, name));
+        throw new MissingComponentException();
+    }
+
+    public TileEdge Backward(TileEdge heading)
+    {
+        return Forward(heading);
+    }
+
+    public void Bump(TileEdge fromDirection)
+    {
+        //TODO: handle burn
+    }
+
+    float gizmoCubeSize = 0.15f;
+    private void OnDrawGizmosSelected()
+    {
+        bool emptyExit = false;
+        for (int i = 0;  i < exits.Length; i++)
+        {
+            if (exits[i] == null)
+            {
+                emptyExit = true;
+            }
+            else
+            {                
+                if (exits[i].ExitMode == TileEdgeMode.Fall)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(restPosition.position, exits[i].transform.position);
+                    Gizmos.DrawCube(exits[i].transform.position, Vector3.one * gizmoCubeSize);
+                } else if (exits[i].ExitMode == TileEdgeMode.Block)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(transform.position, exits[i].transform.position);
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(exits[i].transform.position, Vector3.one * gizmoCubeSize);
+                } else if (exits[i].ExitMode == TileEdgeMode.Allow)
+                {
+                    Gizmos.color = Color.cyan;
+                    Gizmos.DrawLine(transform.position, exits[i].transform.position);
+                    Gizmos.DrawCube(exits[i].transform.position, Vector3.one * gizmoCubeSize);
+                    Gizmos.DrawCube(exits[i].Connection.transform.position, Vector3.one * gizmoCubeSize);
+                    Gizmos.DrawLine(exits[i].Connection.transform.position, exits[i].ConnectedTile.restPosition.transform.position);
+                    Gizmos.DrawCube(exits[i].ConnectedTile.RestPosition.transform.position, Vector3.one * gizmoCubeSize);
+                }
+                
+            }
+        }
+        if (emptyExit)
+        {
+            Gizmos.color = Color.red;
+        } else
+        {
+            Gizmos.color = Color.black;
+        }
+        Gizmos.DrawCube(RestPosition.position, Vector3.one * gizmoCubeSize);
+    }
+#if UNITY_EDITOR
+    private void FixEdgeConnections()
+    {
+        for (int i=0, l=entries.Length; i<l; i++)
+        {
+            entries[i].AutoConnect();
+        }
+
+        for (int i = 0, l = exits.Length; i < l; i++) {
+            exits[i].AutoConnect();
+        }
+    }
+
+    private void Update()
+    {
+        if (FixEdges)
+        {
+            FixEdges = false;
+            FixEdgeConnections();            
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/Tile/TileEdge.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEdge : MonoBehaviour
+{
+    [SerializeField] TileEdge connection;
+    [SerializeField] TileEdgeMode entryMode = TileEdgeMode.Allow;
+    Tile _tile;
+    Tile tile
+    {
+        get
+        {
+            if (_tile == null)
+            {
+                _tile = GetComponentInParent<Tile>();
+            }
+            return _tile;
+        }
+    }
+
+    public Tile ConnectedTile
+    {
+        get { return connection?.tile; }
+    }
+    public TileEdge Connection
+    {
+        get { return connection; }
+    }
+
+    public TileEdgeMode ExitMode
+    {
+        get
+        {
+            if (connection == null) return TileEdgeMode.Fall;
+            return connection.entryMode;
+        }
+    }
+
+    public void BumpConnected()
+    {
+        connection?.tile.Bump(connection);
+    }
+
+    public TileEdge HeadingAfterPassing
+    {
+        get
+        {
+            return connection?.tile.Forward(connection);
+        }
+    }
+
+#if UNITY_EDITOR
+    [Header("Autoconnect")]
+    [SerializeField]
+    float connectToleranceSq = 0.05f;
+    [SerializeField]
+    float disconnectToleranceSq = 0.1f;
+    public void AutoConnect()
+    {
+        if (connection != null)
+        {
+            if ((transform.position - connection.transform.position).sqrMagnitude > disconnectToleranceSq)
+            {
+                connection = null;
+            }
+        } else
+        {
+            var edges = FindObjectsOfType<TileEdge>();
+            for (int i = 0; i< edges.Length; i++)
+            {
+                if (edges[i] == this || (edges[i].transform.position - transform.position).sqrMagnitude > connectToleranceSq) continue;
+                edges[i].connection = this;
+                connection = edges[i];
+                break;
+            }
+        }
+    }
+#endif
+}
