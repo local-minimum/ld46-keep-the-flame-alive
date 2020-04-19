cdb87c18d4c2b668c1f4ac7d02515cc4d4a04ee2
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-04-19 10:58:10 +0200

    A basic game camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] RobotController robot;
+    Vector3 offset;
+    Vector3 velocity;
+    float minY;
+    [SerializeField] float smoothTime = 0.5f; 
+    private void Start()
+    {
+        minY = transform.position.y;
+        offset = transform.position - robot.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 target = robot.transform.position + offset;
+        target.y = Mathf.Max(target.y, minY);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
+        transform.LookAt(robot.transform);
+    }
+}
